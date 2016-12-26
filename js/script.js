36b5c5fb2625b0fecea0f990f5653c0230db254b
Ильшат Кильдияров <ilshatkin@mail.ru>
2016-12-26 23:07:49 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ navToggle.addEventListener("click", function() {
         navMain.classList.remove("main-nav--opened");
     }
 });
+
+
+var link = document.querySelector(".product-of-week__btn");
+var popup = document.querySelector(".modal-content");
+var overlay = document.querySelector(".modal-overlay");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content--show");
+    overlay.classList.add("modal-overlay--show");
+});
+
+overlay.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content--show");
+    overlay.classList.remove("modal-overlay--show");
+});
